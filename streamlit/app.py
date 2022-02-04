a9aac7ce2771efd63f767d2cfef3a04551715fc9
Vedant Rishi Das <vedantdas130701@gmail.com>
2022-02-05 00:15:01 +0530

    updated streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -5,11 +5,25 @@ import folium
 import pandas as pd
 from locationHERE import getCoordinates
 from map import make_map
+import plotly.graph_objects as go
+
+
 def app():
+	df = pd.read_csv("locData.csv")
 	loc = st.text_input('Enter location')
+	fig = go.Figure(data=[go.Table(
+    header=dict(values=list(df.columns),
+                fill_color='#273346',
+                align='left'),
+    cells=dict(values=[df.index,df.location, df.score],
+               fill_color='#273346',
+               align='left'))
+	])
+	# fig.update_layout(width = 800)
+	st.write(fig)
+	# st.table(df)
 	if loc:
 		coordinates = getCoordinates(loc)
-		coordinates
 		emp = st.empty()
 		with emp.container():
 			st.title("Map")
@@ -17,7 +31,6 @@ def app():
 			with placeholder_map.container():
 				main_map = make_map(coordinates['coordinates'])
 				folium_static(main_map)
-    
 
 if __name__ == '__main__':
 	app()
